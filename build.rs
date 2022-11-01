8318e76f69a742116790ee9b164306985be937f7
Max Coppen <44031065+mxcop@users.noreply.github.com>
2022-11-01 17:53:10 +0100

    fix: windows build command

--- build.rs
+++ build.rs
@@ -1,17 +1,29 @@
 use std::process::Command;
 
 fn main() {
-    Command::new("esbuild")
-        .args([
-            "--outdir=dist",
-            "--format=esm",
-            "--bundle",
-            "tauri/tooling/api/src/app.ts",
-            "tauri/tooling/api/src/clipboard.ts",
-            "tauri/tooling/api/src/tauri.ts",
-            "tauri/tooling/api/src/event.ts",
-            "tauri/tooling/api/src/mocks.ts",
-        ])
-        .output()
-        .unwrap();
+    /* Shared arguments */
+    let sargs: [&str; 8] = [
+        "--outdir=dist",
+        "--format=esm",
+        "--bundle",
+        "tauri/tooling/api/src/app.ts",
+        "tauri/tooling/api/src/clipboard.ts",
+        "tauri/tooling/api/src/tauri.ts",
+        "tauri/tooling/api/src/event.ts",
+        "tauri/tooling/api/src/mocks.ts",
+    ];
+
+    if cfg!(target_os = "windows") {
+        /* Use cmd if the target is windows */
+        Command::new("cmd")
+            .args(&["/C", "esbuild"])
+            .args(&sargs)
+            .output()
+            .unwrap();
+    } else {
+        Command::new("esbuild")
+            .args(&sargs)
+            .output()
+            .unwrap();
+    };
 }
