91dbdb3df99e824cd4361d37069c8ae3f42d45ba
Fermin Cirella <fermincirella.ar@gmail.com>
2023-09-07 13:10:54 -0300

    Add support for new v2 App plugin

--- src/app.js
+++ src/app.js
@@ -1,91 +1,21 @@
-// tauri/tooling/api/src/tauri.ts
-function uid() {
-  return window.crypto.getRandomValues(new Uint32Array(1))[0];
-}
-function transformCallback(callback, once = false) {
-  const identifier = uid();
-  const prop = `_${identifier}`;
-  Object.defineProperty(window, prop, {
-    value: (result) => {
-      if (once) {
-        Reflect.deleteProperty(window, prop);
-      }
-      return callback?.(result);
-    },
-    writable: false,
-    configurable: true
-  });
-  return identifier;
-}
-async function invoke(cmd, args = {}) {
-  return new Promise((resolve, reject) => {
-    const callback = transformCallback((e) => {
-      resolve(e);
-      Reflect.deleteProperty(window, `_${error}`);
-    }, true);
-    const error = transformCallback((e) => {
-      reject(e);
-      Reflect.deleteProperty(window, `_${callback}`);
-    }, true);
-    window.__TAURI_IPC__({
-      cmd,
-      callback,
-      error,
-      ...args
-    });
-  });
-}
-
-// tauri/tooling/api/src/helpers/tauri.ts
-async function invokeTauriCommand(command) {
-  return invoke("tauri", command);
-}
-
-// tauri/tooling/api/src/app.ts
 async function getVersion() {
-  return invokeTauriCommand({
-    __tauriModule: "App",
-    message: {
-      cmd: "getAppVersion"
-    }
-  });
+  return window.__TAURI_INVOKE__("plugin:app|version");
 }
+
 async function getName() {
-  return invokeTauriCommand({
-    __tauriModule: "App",
-    message: {
-      cmd: "getAppName"
-    }
-  });
+  return window.__TAURI_INVOKE__("plugin:app|name");
 }
+
 async function getTauriVersion() {
-  return invokeTauriCommand({
-    __tauriModule: "App",
-    message: {
-      cmd: "getTauriVersion"
-    }
-  });
+  return window.__TAURI_INVOKE__("plugin:app|tauri_version");
 }
+
 async function show() {
-  return invokeTauriCommand({
-    __tauriModule: "App",
-    message: {
-      cmd: "show"
-    }
-  });
+  return window.__TAURI_INVOKE__("plugin:app|show");
 }
+
 async function hide() {
-  return invokeTauriCommand({
-    __tauriModule: "App",
-    message: {
-      cmd: "hide"
-    }
-  });
+  return window.__TAURI_INVOKE__("plugin:app|hide");
 }
-export {
-  getName,
-  getTauriVersion,
-  getVersion,
-  hide,
-  show
-};
+
+export { getName, getVersion, getTauriVersion, show, hide };
