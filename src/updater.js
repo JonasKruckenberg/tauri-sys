c00057d1738445c42bf112420f5f6c5b5a4b8527
Fermin Cirella <fermincirella.ar@gmail.com>
2023-09-08 03:48:41 -0300

    Initial support for v2 Updater

--- src/updater.js
+++ src/updater.js
@@ -1,185 +1,63 @@
-// tauri/tooling/api/src/tauri.ts
-function uid() {
-  return window.crypto.getRandomValues(new Uint32Array(1))[0];
-}
-function transformCallback(callback, once3 = false) {
-  const identifier = uid();
-  const prop = `_${identifier}`;
-  Object.defineProperty(window, prop, {
-    value: (result) => {
-      if (once3) {
-        Reflect.deleteProperty(window, prop);
-      }
-      return callback?.(result);
-    },
-    writable: false,
-    configurable: true
-  });
-  return identifier;
-}
-async function invoke(cmd, args = {}) {
-  return new Promise((resolve, reject) => {
-    const callback = transformCallback((e) => {
-      resolve(e);
-      Reflect.deleteProperty(window, `_${error}`);
-    }, true);
-    const error = transformCallback((e) => {
-      reject(e);
-      Reflect.deleteProperty(window, `_${callback}`);
-    }, true);
-    window.__TAURI_IPC__({
-      cmd,
-      callback,
-      error,
-      ...args
+class Channel {
+  id;
+  __TAURI_CHANNEL_MARKER__ = true;
+  #onmessage = () => {};
+
+  constructor() {
+    this.id = window.__TAURI__.transformCallback((response) => {
+      this.#onmessage(response);
     });
-  });
-}
-
-// tauri/tooling/api/src/helpers/tauri.ts
-async function invokeTauriCommand(command) {
-  return invoke("tauri", command);
-}
-
-// tauri/tooling/api/src/helpers/event.ts
-async function _unlisten(event, eventId) {
-  return invokeTauriCommand({
-    __tauriModule: "Event",
-    message: {
-      cmd: "unlisten",
-      event,
-      eventId
-    }
-  });
-}
-async function emit(event, windowLabel, payload) {
-  await invokeTauriCommand({
-    __tauriModule: "Event",
-    message: {
-      cmd: "emit",
-      event,
-      windowLabel,
-      payload
-    }
-  });
-}
-async function listen(event, windowLabel, handler) {
-  return invokeTauriCommand({
-    __tauriModule: "Event",
-    message: {
-      cmd: "listen",
-      event,
-      windowLabel,
-      handler: transformCallback(handler)
-    }
-  }).then((eventId) => {
-    return async () => _unlisten(event, eventId);
-  });
-}
-async function once(event, windowLabel, handler) {
-  return listen(event, windowLabel, (eventData) => {
-    handler(eventData);
-    _unlisten(event, eventData.id).catch(() => {
-    });
-  });
-}
-
-// tauri/tooling/api/src/event.ts
-async function listen2(event, handler) {
-  return listen(event, null, handler);
-}
-async function once2(event, handler) {
-  return once(event, null, handler);
-}
-async function emit2(event, payload) {
-  return emit(event, void 0, payload);
-}
-
-// tauri/tooling/api/src/updater.ts
-async function onUpdaterEvent(handler) {
-  return listen2("tauri://update-status" /* STATUS_UPDATE */, (data) => {
-    handler(data?.payload);
-  });
-}
-async function installUpdate() {
-  let unlistenerFn;
-  function cleanListener() {
-    if (unlistenerFn) {
-      unlistenerFn();
-    }
-    unlistenerFn = void 0;
   }
-  return new Promise((resolve, reject) => {
-    function onStatusChange(statusResult) {
-      if (statusResult.error) {
-        cleanListener();
-        return reject(statusResult.error);
-      }
-      if (statusResult.status === "DONE") {
-        cleanListener();
-        return resolve();
-      }
-    }
-    onUpdaterEvent(onStatusChange).then((fn) => {
-      unlistenerFn = fn;
-    }).catch((e) => {
-      cleanListener();
-      throw e;
-    });
-    emit2("tauri://update-install" /* INSTALL_UPDATE */).catch((e) => {
-      cleanListener();
-      throw e;
-    });
-  });
-}
-async function checkUpdate() {
-  let unlistenerFn;
-  function cleanListener() {
-    if (unlistenerFn) {
-      unlistenerFn();
-    }
-    unlistenerFn = void 0;
+
+  set onmessage(handler) {
+    this.#onmessage = handler;
   }
-  return new Promise((resolve, reject) => {
-    function onUpdateAvailable(manifest) {
-      cleanListener();
-      return resolve({
-        manifest,
-        shouldUpdate: true
-      });
-    }
-    function onStatusChange(statusResult) {
-      if (statusResult.error) {
-        cleanListener();
-        return reject(statusResult.error);
-      }
-      if (statusResult.status === "UPTODATE") {
-        cleanListener();
-        return resolve({
-          shouldUpdate: false
-        });
-      }
-    }
-    once2("tauri://update-available" /* UPDATE_AVAILABLE */, (data) => {
-      onUpdateAvailable(data?.payload);
-    }).catch((e) => {
-      cleanListener();
-      throw e;
-    });
-    onUpdaterEvent(onStatusChange).then((fn) => {
-      unlistenerFn = fn;
-    }).catch((e) => {
-      cleanListener();
-      throw e;
-    });
-    emit2("tauri://update" /* CHECK_UPDATE */).catch((e) => {
-      cleanListener();
-      throw e;
-    });
-  });
+
+  get onmessage() {
+    return this.#onmessage;
+  }
+
+  toJSON() {
+    return `__CHANNEL__:${this.id}`;
+  }
 }
-export {
-  checkUpdate,
-  installUpdate,
-  onUpdaterEvent
-};
+
+class Update {
+  available;
+  currentVersion;
+  version;
+  date;
+  body;
+
+  constructor(metadata) {
+    this.available = metadata.available;
+    this.currentVersion = metadata.currentVersion;
+    this.version = metadata.version;
+    this.date = metadata.date;
+    this.body = metadata.body;
+  }
+
+  async downloadAndInstall(onEvent) {
+    const channel = new Channel();
+
+    if (onEvent != null) {
+      channel.onmessage = onEvent;
+    }
+
+    return window.__TAURI_INVOKE__("plugin:updater|download_and_install", {
+      onEvent: channel,
+    });
+  }
+}
+
+async function check(options) {
+  if (options?.headers) {
+    options.headers = Array.from(new Headers(options.headers).entries());
+  }
+
+  return window
+    .__TAURI_INVOKE__("plugin:updater|check", { ...options })
+    .then((meta) => (meta.available ? new Update(meta) : null));
+}
+
+export { Update, check };
--- src/updater.rs
+++ src/updater.rs
@@ -1,139 +1,83 @@
-//! Customize the auto updater flow.
+use std::{cell::RefCell, rc::Rc};
 
-use futures::{Stream, channel::mpsc};
-use serde::Deserialize;
+use futures::{channel::mpsc, Stream, StreamExt};
+use serde::{Deserialize, Serialize};
 use wasm_bindgen::{prelude::Closure, JsValue};
-use crate::event::Listen;
 
-#[derive(Deserialize, Debug, Clone)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
 pub struct UpdateManifest {
+    pub available: bool,
     pub body: String,
+    pub current_version: String,
     pub date: String,
     pub version: String,
 }
 
-#[derive(Deserialize, Debug, Clone)]
-#[serde(rename_all = "camelCase")]
-pub struct UpdateResult {
-    pub manifest: Option<UpdateManifest>,
-    pub should_update: bool,
+#[derive(Debug, Clone, PartialEq)]
+pub struct Update(inner::Update);
+
+impl Update {
+    #[inline(always)]
+    pub async fn download_and_install(&self) -> crate::Result<impl Stream<Item = ()>> {
+        let (tx, rx) = mpsc::unbounded();
+
+        let closure = Closure::<dyn FnMut(JsValue)>::new(move |_| {
+            let _ = tx.unbounded_send(());
+        });
+
+        self.0.downloadAndInstall(&closure).await?;
+
+        closure.forget();
+
+        Ok(Listen { rx })
+    }
 }
 
-#[derive(Deserialize)]
-struct UpdateStatusResult {
-    error: Option<String>,
-    status: UpdateStatus,
-}
-
-#[derive(Deserialize)]
-pub enum UpdateStatus {
-    #[serde(rename = "PENDING")]
-    Pending,
-    #[serde(rename = "DONE")]
-    Done,
-    #[serde(rename = "UPTODATE")]
-    UpToDate,
-}
-
-/// Checks if an update is available.
-///
-/// # Example
-///
-/// ```rust,no_run
-/// use tauri_sys::updater::check_update;
-///
-/// # async fn main() -> Result<(), Box<dyn std::error::Error>> {
-/// let update = check_update().await?;
-/// // now run installUpdate() if needed
-/// # Ok(())
-/// # }
-/// ```
 #[inline(always)]
-pub async fn check_update() -> crate::Result<UpdateResult> {
-    let raw = inner::checkUpdate().await?;
+pub async fn check() -> crate::Result<Update> {
+    let raw = inner::check().await?;
 
-    Ok(serde_wasm_bindgen::from_value(raw)?)
+    Ok(Update(inner::Update::new(raw)))
 }
 
-/// Install the update if there's one available.
-///
-/// # Example
-///
-/// ```rust,no_run
-/// use tauri_sys::updater::{check_update, install_update};
-///
-/// # async fn main() -> Result<(), Box<dyn std::error::Error>> {
-/// let update = check_update().await?;
-///
-/// if update.should_update {
-///     log::info("Installing update {:?}", update.manifest);
-///     install_update().await?;
-/// }
-/// # Ok(())
-/// # }
-/// ```
-#[inline(always)]
-pub async fn install_update() -> crate::Result<()> {
-    inner::installUpdate().await?;
-    Ok(())
+struct Listen<T> {
+    pub rx: mpsc::UnboundedReceiver<T>,
 }
 
-/// Listen to an updater event.
-///
-/// The returned Future will automatically clean up it's underlying event listener when dropped, so no manual unlisten function needs to be called.
-/// See [Differences to the JavaScript API](../index.html#differences-to-the-javascript-api) for details.
-/// 
-/// # Example
-///
-/// ```rust,no_run
-/// use tauri_sys::updater::updater_events;
-/// use web_sys::console;
-///
-/// # async fn main() -> Result<(), Box<dyn std::error::Error>> {
-/// let events = updater_events();
-///
-/// while let Some(event) = events.next().await {
-///     console::log_1(&format!("Updater event {:?}", event).into());
-/// }
-/// # Ok(())
-/// # }
-/// ```
-#[inline(always)]
-pub async fn updater_events() -> crate::Result<impl Stream<Item = Result<UpdateStatus, String>>> {
-    let (tx, rx) = mpsc::unbounded::<Result<UpdateStatus, String>>();
+impl<T> Stream for Listen<T> {
+    type Item = T;
 
-    let closure = Closure::<dyn FnMut(JsValue)>::new(move |raw| {
-        let raw: UpdateStatusResult = serde_wasm_bindgen::from_value(raw).unwrap();
-
-        let msg = if let Some(error) = raw.error {
-            Err(error)
-        } else {
-            Ok(raw.status)
-        };
-
-        let _ = tx.unbounded_send(msg);
-    });
-    let unlisten = inner::onUpdaterEvent(&closure).await?;
-    closure.forget();
-
-    Ok(Listen {
-        rx,
-        unlisten: js_sys::Function::from(unlisten),
-    })
+    fn poll_next(
+        mut self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Option<Self::Item>> {
+        self.rx.poll_next_unpin(cx)
+    }
 }
 
 mod inner {
-    use wasm_bindgen::prelude::*;
+    use wasm_bindgen::{
+        prelude::{wasm_bindgen, Closure},
+        JsValue,
+    };
+
+    #[wasm_bindgen(module = "/src/updater.js")]
+    extern "C" {
+        #[derive(Debug, Clone, PartialEq)]
+        pub type Update;
+        #[wasm_bindgen(constructor)]
+        pub fn new(metadata: JsValue) -> Update;
+        #[wasm_bindgen(method, catch)]
+        pub async fn downloadAndInstall(
+            this: &Update,
+            handler: &Closure<dyn FnMut(JsValue)>,
+        ) -> Result<JsValue, JsValue>;
+    }
 
     #[wasm_bindgen(module = "/src/updater.js")]
     extern "C" {
         #[wasm_bindgen(catch)]
-        pub async fn checkUpdate() -> Result<JsValue, JsValue>;
-        #[wasm_bindgen(catch)]
-        pub async fn installUpdate() -> Result<JsValue, JsValue>;
-        #[wasm_bindgen(catch)]
-        pub async fn onUpdaterEvent(
-            handler: &Closure<dyn FnMut(JsValue)>,
-        ) -> Result<JsValue, JsValue>;
+        pub async fn check() -> Result<JsValue, JsValue>;
     }
 }
