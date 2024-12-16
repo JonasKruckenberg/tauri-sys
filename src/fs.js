dc0c8bd0176a1e296f166a94a1438a7f2343e70c
Lewin Probst <info@emirror.de>
2024-12-16 15:06:34 +0100

    chore: Replaced Resource class definition with window.__TAURI__.core.Resource

--- src/fs.js
+++ src/fs.js
@@ -179,46 +179,6 @@ async function requestPermissions(plugin) {
 function convertFileSrc(filePath, protocol = 'asset') {
     return window.__TAURI_INTERNALS__.convertFileSrc(filePath, protocol);
 }
-/**
- * A rust-backed resource stored through `tauri::Manager::resources_table` API.
- *
- * The resource lives in the main process and does not exist
- * in the Javascript world, and thus will not be cleaned up automatiacally
- * except on application exit. If you want to clean it up early, call {@linkcode Resource.close}
- *
- * @example
- * ```typescript
- * import { Resource, invoke } from '@tauri-apps/api/core';
- * export class DatabaseHandle extends Resource {
- *   static async open(path: string): Promise<DatabaseHandle> {
- *     const rid: number = await invoke('open_db', { path });
- *     return new DatabaseHandle(rid);
- *   }
- *
- *   async execute(sql: string): Promise<void> {
- *     await invoke('execute_sql', { rid: this.rid, sql });
- *   }
- * }
- * ```
- */
-class Resource {
-    get rid() {
-        return __classPrivateFieldGet(this, _Resource_rid, "f");
-    }
-    constructor(rid) {
-        _Resource_rid.set(this, void 0);
-        __classPrivateFieldSet(this, _Resource_rid, rid, "f");
-    }
-    /**
-     * Destroys and cleans up this resource from memory.
-     * **You should not call any method on this object anymore and should drop any reference to it.**
-     */
-    async close() {
-        return invoke('plugin:resources|close', {
-            rid: this.rid
-        });
-    }
-}
 _Resource_rid = new WeakMap();
 function isTauri() {
     return 'isTauri' in window && !!window.isTauri;
@@ -344,7 +304,7 @@ function fromBytes(buffer) {
  *
  * @since 2.0.0
  */
-class FileHandle extends Resource {
+class FileHandle extends window.__TAURI__.core.Resource {
     /**
      * Reads up to `p.byteLength` bytes into `p`. It resolves to the number of
      * bytes read (`0` < `n` <= `p.byteLength`) and rejects if any error
